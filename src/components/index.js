b598e211856f4d56f2627bd89e070c812ef4b855
Alin Voinea <contact@avoinea.com>
2020-08-24 14:20:33 +0300

    DataFigure Block based on Volto Image

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+/**
+ * Point of contact for component modules.
+ * @module components
+ */
+export EditDataFigureBlock from './manage/Blocks/DataFigure/Edit';
+export ViewDataFigureBlock from './manage/Blocks/DataFigure/View';
--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
+import { EditDataFigureBlock } from './components';
+import { ViewDataFigureBlock } from './components';
+import iconSVG from '@plone/volto/icons/image.svg';
+
 const applyConfig = (config) => {
+  config.blocks.blocksConfig.dataFigure = {
+    id: 'dataFigure',
+    title: 'Data Figure',
+    icon: iconSVG,
+    group: 'media',
+    view: ViewDataFigureBlock,
+    edit: EditDataFigureBlock,
+    restricted: false,
+    mostUsed: false,
+    blockHasOwnFocusManagement: false,
+    sidebarTab: 1,
+    security: {
+      addPermission: [],
+      view: [],
+    },
+  };
+
   return config;
 };
 

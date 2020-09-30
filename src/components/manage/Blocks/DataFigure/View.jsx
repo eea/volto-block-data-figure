318d74736c98308bf6eed3ae6f35776d0eafd1d4
nileshgulia1 <nileshgulia@gmail.com>
2020-09-30 21:47:59 +0530

    do not use transition at all to avoid side effects

--- src/components/manage/Blocks/DataFigure/View.jsx
+++ src/components/manage/Blocks/DataFigure/View.jsx
@@ -4,10 +4,13 @@
  */
 import './less/public.less';
 import Png from './Png';
+import cx from 'classnames';
+import { settings } from '@plone/volto/config';
 import PropTypes from 'prop-types';
-import { Transition, Button, Divider } from 'semantic-ui-react';
+import { Button, Divider } from 'semantic-ui-react';
 import replaceSVG from '@plone/volto/icons/replace.svg';
 import { Icon } from '@plone/volto/components';
+import { flattenToAppURL } from '@plone/volto/helpers';
 import React from 'react';
 import Table from './Table';
 
@@ -19,7 +22,7 @@ import Table from './Table';
 
 class View extends React.Component {
   state = {
-    visible: true,
+    visible: false,
   };
 
   toggleVisibility = () =>
@@ -30,32 +33,33 @@ class View extends React.Component {
     const { data, detached } = this.props;
     return this.props.data.url?.includes('.svg') ? (
       <div>
-        <Transition.Group
-          visible={visible}
-          animation="horizontal flip"
-          duration={500}
-          unmountOnHide={false}
-        >
-          {visible && (
-            <img
-              src={data.url}
-              style={{ width: '100%', height: '100%' }}
-              detached={detached}
-              alt={data.alt}
-            />
-          )}
-          {!visible && (
-            <div
-              style={{
-                width: '100%',
-                height: '500px',
-                overflowY: 'scroll',
-              }}
-            >
+        <div className="scene scene--card">
+          <div className={`card ${visible ? ' is-flipped' : ''}`}>
+            <div className="card__face card__face--front">
+              <img
+                className={cx({ 'full-width': data.align === 'full' })}
+                style={{
+                  width: data.width ? data.width + 'px' : '100%',
+                  height: data.height ? data.height + 'px' : '100%',
+                  marginLeft:
+                    data.inLeftColumn && data.width
+                      ? `-${parseInt(data.width) + 10}px`
+                      : '0',
+                  marginRight: data.inLeftColumn ? '0!important' : '1rem',
+                }}
+                src={
+                  data.url.includes(settings.apiPath)
+                    ? `${flattenToAppURL(data.url)}/@@images/image`
+                    : data.url
+                }
+                alt={data.alt || ''}
+              ></img>
+            </div>
+            <div className="card__face card__face--back">
               <Table data={data} />
             </div>
-          )}
-        </Transition.Group>
+          </div>
+        </div>
         <Divider hidden />
         <Button
           icon={<Icon name={replaceSVG} />}

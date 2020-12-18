13e5c693bae23e6d1b98cc3d47ab0503d5940057
nileshgulia1 <nileshgulia@gmail.com>
2020-12-18 23:30:17 +0530

    add cypress tests

--- cypress/integration/blocks-data-figure.js
+++ cypress/integration/blocks-data-figure.js
@@ -51,4 +51,28 @@ describe('Blocks Tests', () => {
     // then the page view should contain our changes
     cy.contains('My Add-on Page');
   });
+  it('Add Data Figure with Daviz url', () => {
+    const davizUrl =
+      'https://www.eea.europa.eu/cypress/change-in-sulphur-oxides-emissions';
+    // Change page title
+    cy.get('.documentFirstHeading > .public-DraftStyleDefault-block')
+      .clear()
+      .type('My Add-on Page')
+      .get('.documentFirstHeading span[data-text]')
+      .contains('My Add-on Page');
+
+    cy.get('.documentFirstHeading > .public-DraftStyleDefault-block').type(
+      '{enter}',
+    );
+    cy.get('.ui.basic.icon.button.block-add-button').first().click();
+    cy.get('.blocks-chooser .title').contains('Media').click();
+    cy.get('.ui.basic.icon.button.dataFigure').contains('Data Figure').click();
+    cy.get('.ui:nth-child(3) > input').click();
+    cy.get('.ui:nth-child(3) > input').type(davizUrl);
+    cy.get('.primary > .icon').click();
+    cy.waitForResourceToLoad(davizUrl);
+    cy.get('div.block.image').find('img').should('be.visible');
+    cy.get('#toolbar-save > .icon').click();
+    cy.get('img').should('have.attr', 'src');
+  });
 });

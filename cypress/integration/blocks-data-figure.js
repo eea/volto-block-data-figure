f766bb080f6f0c7b06f1c2a9e1dbfdf1627f7bc0
nileshgulia1 <nileshgulia@gmail.com>
2020-12-18 23:09:18 +0530

    test only daviz URls

--- cypress/integration/blocks-data-figure.js
+++ cypress/integration/blocks-data-figure.js
@@ -75,30 +75,4 @@ describe('Blocks Tests', () => {
     cy.get('#toolbar-save > .icon').click();
     cy.get('img').should('have.attr', 'src');
   });
-  it('Add Data Figure with static figures', () => {
-    const staticUrl =
-      'https://eea.europa.eu/SITE/data-and-maps/figures/the-average-summer-season-intensity';
-    // Change page title
-    cy.get('.documentFirstHeading > .public-DraftStyleDefault-block')
-      .clear()
-      .type('My Add-on Page')
-      .get('.documentFirstHeading span[data-text]')
-      .contains('My Add-on Page');
-
-    cy.get('.documentFirstHeading > .public-DraftStyleDefault-block').type(
-      '{enter}',
-    );
-    cy.get('.ui.basic.icon.button.block-add-button').first().click();
-    cy.get('.blocks-chooser .title').contains('Media').click();
-    cy.get('.ui.basic.icon.button.dataFigure').contains('Data Figure').click();
-    cy.get('.ui:nth-child(3) > input').click();
-    cy.get('.ui:nth-child(3) > input').type(staticUrl);
-    cy.get('.primary > .icon').click();
-    cy.server();
-    cy.route('GET', staticUrl, 'fix:example');
-    cy.waitForResourceToLoad(200);
-    cy.get('div.block.image').find('img').should('be.visible');
-    cy.get('#toolbar-save > .icon').click();
-    cy.get('img').should('be.visible');
-  });
 });

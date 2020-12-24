a7ebe375128b94ba1bbcc330bf3241798078c967
nileshgulia1 <nileshgulia@gmail.com>
2020-12-24 21:35:14 +0530

    rearranged static figure tests

--- cypress/integration/blocks-data-figure.js
+++ cypress/integration/blocks-data-figure.js
@@ -42,85 +42,85 @@ describe('Blocks Tests', () => {
     cy.autologin();
     cy.removeContent('cypress');
   });
-  it('Add Data Figure with static figures', () => {
-    const staticUrl =
-      'https://eea.europa.eu/SITE/data-and-maps/figures/the-average-summer-season-intensity';
-    // Change page title
-    cy.get('.documentFirstHeading > .public-DraftStyleDefault-block')
-      .clear()
-      .type('My Add-on Page')
-      .get('.documentFirstHeading span[data-text]')
-      .contains('My Add-on Page');
+});
+it('Add Data Figure block: Empty', () => {
+  // Change page title
+  cy.get('.documentFirstHeading > .public-DraftStyleDefault-block')
+    .clear()
+    .type('My Add-on Page')
+    .get('.documentFirstHeading span[data-text]')
+    .contains('My Add-on Page');
 
-    cy.get('.documentFirstHeading > .public-DraftStyleDefault-block').type(
-      '{enter}',
-    );
-    cy.get('.ui.basic.icon.button.block-add-button').first().click();
-    cy.get('.blocks-chooser .title').contains('Media').click();
-    cy.get('.ui.basic.icon.button.dataFigure').contains('Data Figure').click();
-    cy.get('.ui:nth-child(3) > input').click();
-    cy.get('.ui:nth-child(3) > input').type(staticUrl);
-    cy.get('.primary > .icon').click();
+  cy.get('.documentFirstHeading > .public-DraftStyleDefault-block').type(
+    '{enter}',
+  );
 
-    cy.get('div.block.image')
-      .find('img')
-      .invoke('width')
-      .should('be.greaterThan', 0);
+  // Add metadata block
+  cy.get('.ui.basic.icon.button.block-add-button').first().click();
+  cy.get('.blocks-chooser .title').contains('Media').click();
+  cy.get('.ui.basic.icon.button.dataFigure').contains('Data Figure').click();
 
-    cy.get('div.block.image').find('img').should('be.visible');
-    cy.get('#toolbar-save > .icon')
-      .click()
-      .then(() => {
-        cy.wait(1000);
-        cy.get('.scene img').should('be.visible');
-      });
-  });
-  it('Add Data Figure block: Empty', () => {
-    // Change page title
-    cy.get('.documentFirstHeading > .public-DraftStyleDefault-block')
-      .clear()
-      .type('My Add-on Page')
-      .get('.documentFirstHeading span[data-text]')
-      .contains('My Add-on Page');
+  // Save
+  cy.get('#toolbar-save').click();
+  cy.url().should('eq', Cypress.config().baseUrl + '/cypress/my-page');
 
-    cy.get('.documentFirstHeading > .public-DraftStyleDefault-block').type(
-      '{enter}',
-    );
+  // then the page view should contain our changes
+  cy.contains('My Add-on Page');
+});
+it('Add Data Figure with Daviz url', () => {
+  const davizUrl =
+    'https://www.eea.europa.eu/cypress/change-in-sulphur-oxides-emissions';
+  // Change page title
+  cy.get('.documentFirstHeading > .public-DraftStyleDefault-block')
+    .clear()
+    .type('My Add-on Page')
+    .get('.documentFirstHeading span[data-text]')
+    .contains('My Add-on Page');
 
-    // Add metadata block
-    cy.get('.ui.basic.icon.button.block-add-button').first().click();
-    cy.get('.blocks-chooser .title').contains('Media').click();
-    cy.get('.ui.basic.icon.button.dataFigure').contains('Data Figure').click();
+  cy.get('.documentFirstHeading > .public-DraftStyleDefault-block').type(
+    '{enter}',
+  );
+  cy.get('.ui.basic.icon.button.block-add-button').first().click();
+  cy.get('.blocks-chooser .title').contains('Media').click();
+  cy.get('.ui.basic.icon.button.dataFigure').contains('Data Figure').click();
+  cy.get('.ui:nth-child(3) > input').click();
+  cy.get('.ui:nth-child(3) > input').type(davizUrl);
+  cy.get('.primary > .icon').click();
+  cy.waitForResourceToLoad(davizUrl);
+  cy.get('div.block.image').find('img').should('be.visible');
+  cy.get('#toolbar-save > .icon').click();
+  cy.get('img').should('have.attr', 'src');
+});
+it('Add Data Figure with static figures', () => {
+  const staticUrl =
+    'https://eea.europa.eu/SITE/data-and-maps/figures/the-average-summer-season-intensity';
+  // Change page title
+  cy.get('.documentFirstHeading > .public-DraftStyleDefault-block')
+    .clear()
+    .type('My Add-on Page')
+    .get('.documentFirstHeading span[data-text]')
+    .contains('My Add-on Page');
 
-    // Save
-    cy.get('#toolbar-save').click();
-    cy.url().should('eq', Cypress.config().baseUrl + '/cypress/my-page');
+  cy.get('.documentFirstHeading > .public-DraftStyleDefault-block').type(
+    '{enter}',
+  );
+  cy.get('.ui.basic.icon.button.block-add-button').first().click();
+  cy.get('.blocks-chooser .title').contains('Media').click();
+  cy.get('.ui.basic.icon.button.dataFigure').contains('Data Figure').click();
+  cy.get('.ui:nth-child(3) > input').click();
+  cy.get('.ui:nth-child(3) > input').type(staticUrl);
+  cy.get('.primary > .icon').click();
 
-    // then the page view should contain our changes
-    cy.contains('My Add-on Page');
-  });
-  it('Add Data Figure with Daviz url', () => {
-    const davizUrl =
-      'https://www.eea.europa.eu/cypress/change-in-sulphur-oxides-emissions';
-    // Change page title
-    cy.get('.documentFirstHeading > .public-DraftStyleDefault-block')
-      .clear()
-      .type('My Add-on Page')
-      .get('.documentFirstHeading span[data-text]')
-      .contains('My Add-on Page');
+  cy.get('div.block.image')
+    .find('img')
+    .invoke('width')
+    .should('be.greaterThan', 0);
 
-    cy.get('.documentFirstHeading > .public-DraftStyleDefault-block').type(
-      '{enter}',
-    );
-    cy.get('.ui.basic.icon.button.block-add-button').first().click();
-    cy.get('.blocks-chooser .title').contains('Media').click();
-    cy.get('.ui.basic.icon.button.dataFigure').contains('Data Figure').click();
-    cy.get('.ui:nth-child(3) > input').click();
-    cy.get('.ui:nth-child(3) > input').type(davizUrl);
-    cy.get('.primary > .icon').click();
-    cy.waitForResourceToLoad(davizUrl);
-    cy.get('div.block.image').find('img').should('be.visible');
-    cy.get('#toolbar-save > .icon').click();
-    cy.get('img').should('have.attr', 'src');
-  });
+  cy.get('div.block.image').find('img').should('be.visible');
+  cy.get('#toolbar-save > .icon')
+    .click()
+    .then(() => {
+      cy.wait(1000);
+      cy.get('.scene img').should('be.visible');
+    });
 });

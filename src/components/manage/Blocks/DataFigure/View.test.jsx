94e24831e4806a36a5c4eb3506edae27bb15c5ff
ana-oprea <anaop3212@gmail.com>
2023-06-20 17:58:16 +0300

    chore: remove unused import - refs #254313

--- src/components/manage/Blocks/DataFigure/View.test.jsx
+++ src/components/manage/Blocks/DataFigure/View.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import View from './View';
 import { Provider } from 'react-intl-redux';
 import configureStore from 'redux-mock-store';

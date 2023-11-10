27760397bfffe69536da069a8d8c7c2f329cb8b1
nileshgulia1 <nileshgulia@gmail.com>
2023-11-10 19:01:02 +0530

    specs: missing unit tests

---
+++ src/helpers/blocks.test.js
@@ -0,0 +1,33 @@
+import { setImageSize } from './blocks';
+
+jest.mock('@eeacms/volto-block-data-figure/helpers', () => ({
+  isInternalContentURL: jest.fn((url) => true),
+}));
+
+describe('setImageSize', () => {
+  it('returns expected image scale URL when an image properties are passed', () => {
+    const image = {
+      '@id': 'http://localhost:3000/image',
+      image: {
+        download: 'http://localhost:3000/image/@@images/image.png',
+        width: 400,
+        height: 400,
+        scales: {
+          preview: {
+            download: 'http://localhost:3000/image/@@images/image-400.png',
+            width: 400,
+            height: 400,
+          },
+        },
+      },
+    };
+    const expectedUrlObj = {
+      download: 'http://localhost:3000/image/@@images/image-400.png',
+      width: 400,
+      height: 400,
+    };
+
+    const expectedUrl = '@@images/image-400.png';
+    expect(setImageSize(image, image.image, 'm')).toEqual(expectedUrlObj);
+  });
+});

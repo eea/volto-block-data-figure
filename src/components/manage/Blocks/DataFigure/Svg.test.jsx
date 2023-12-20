8f2a3e64495181dd9245353d1fec8a843e7f66e4
Dobricean Ioan Dorian <dobriceanionut1408@gmail.com>
2023-12-20 13:55:59 +0200

    tests

--- src/components/manage/Blocks/DataFigure/Svg.test.jsx
+++ src/components/manage/Blocks/DataFigure/Svg.test.jsx
@@ -1,5 +1,6 @@
 import Svg from './Svg';
 import { render } from '@testing-library/react';
+
 describe('View component', () => {
   test('renders without crashing', () => {
     render(<Svg data={{ url: 'testUrl' }} />);
